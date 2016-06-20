521748968972f97869289867294a7202dc62c39a
Aneetta <Aneetta>
2016-06-20 22:00:44 +0530

    = Node modules have updated

---
+++ socialAuthentication/authentication.js
@@ -0,0 +1,8 @@
+//Import social authetication helpers here
+
+var facebookHelper= require('facebook.js');
+var googleHelper= require('google.js');
+
+var facebookAppid='1936177029942083';   
+var facebookAppSecret='90fad272dc3f47d4fc302f11a0c72cd9';
+
